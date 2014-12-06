f4eec8aadca3d36eef1bb4d4a568ea27946ca23f
Daria Lamberson <daria@DN51ukli.SUNet>
2014-12-06 02:30:57 -0800

    added minutes past article feature plus gitignore thing i guess

---
+++ src/Project/Features/MinutesAfterArticleFeature.java
@@ -0,0 +1,13 @@
+package Project.Features;
+
+import Project.Comment;
+
+/**
+ * Created by daria on 12/6/14.
+ */
+public class MinutesAfterArticleFeature implements Feature {
+    @Override
+    public double value(Comment comment) {
+        return comment.getTime() / 60;
+    }
+}
